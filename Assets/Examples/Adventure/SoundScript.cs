6ef6971b72fbd61697ea157a8c647aff7e9712f5
Emma Russo <erusso@mmm.edu>
2019-04-29 20:31:03 -0400

    firstdraftgame

---
+++ Assets/Examples/Adventure/SoundScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+
+    public AudioClip collisionSound;
+
+    public AudioSource mySource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collisionSound == null) {
+            Debug.Log("No sound file found!");
+
+        }
+        else {
+            mySource.PlayOneShot(collisionSound);
+        }
+    }
+
+}
