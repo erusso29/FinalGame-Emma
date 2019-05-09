79d59989b795e9c5ba9c74b648c5e2538c105e7d
Emma Russo <erusso@mmm.edu>
2019-05-09 11:04:39 -0400

    sound on bonus smileys

---
+++ Assets/SoundScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+
+    public AudioClip collisionSound;
+
+    public AudioSource mySource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collisionSound == null) {
+            Debug.Log("No sound file found!");
+
+        }
+        else {
+            Debug.Log("PLLLAAY");
+            mySource.enabled = true;
+            mySource.PlayOneShot(collisionSound);
+        }
+    }
+
+}
